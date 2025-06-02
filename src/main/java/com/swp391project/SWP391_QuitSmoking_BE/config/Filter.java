c4be1f18e48bd28a11dd85ccdabadceeff92ecfa
Nguyễn Khánh Ngân <khanhngannguyen122@gmail.com>
2025-06-02 07:39:44 +0700

    folder structure

---
+++ src/main/java/com/swp391project/SWP391_QuitSmoking_BE/config/Filter.java
@@ -0,0 +1,4 @@
+package com.swp391project.SWP391_QuitSmoking_BE.config;
+
+public class Filter {
+}
---
+++ src/main/java/com/swp391project/SWP391_QuitSmoking_BE/entity/Account.java
@@ -0,0 +1,8 @@
+package com.swp391project.SWP391_QuitSmoking_BE.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Account {
+    private long
+}
