78e87b92d749342ae7151471930af4910ccba9ff
iSenity1812 <pvduchuya4@gmail.com>
2025-06-06 18:25:49 +0700

    Update user validation field, update UserController, Repository, Services, Request input

--- src/main/java/com/swp391project/SWP391_QuitSmoking_BE/entity/User.java
+++ src/main/java/com/swp391project/SWP391_QuitSmoking_BE/entity/User.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
@@ -34,7 +35,7 @@ public class User implements UserDetails {
     @NotBlank(message = "Tên người dùng không được để trống") // Không được null và không được rỗng/chỉ chứa khoảng trắng
     @Size(min = 3, max = 50, message = "Tên người dùng phải có từ 3 đến 50 ký tự")
     @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Tên người dùng chỉ được chứa chữ cái, số và dấu gạch dưới")
-    @Column(name = "Username", length = 50, unique = true, nullable = false)
+    @Column(name = "Username", length = 50, nullable = false)
     private String username;
 
     @NotBlank(message = "Email không được để trống")
@@ -45,12 +46,17 @@ public class User implements UserDetails {
 
     @NotBlank(message = "Mật khẩu không được để trống")
     @Column(name = "PasswordHash", nullable = false)
+    @Size(min = 8, message = "Mật khẩu phải có ít nhất 8 ký tự và tối đa 20 ký tự")
     private String passwordHash;
 
     @NotNull(message = "Thời gian tạo không được để trống")
     @PastOrPresent(message = "Thời gian tạo không thể ở tương lai") //LocalDateTime.now() luôn trả về thời gian hiện tại hoặc trong quá khứ
     @Column(name = "CreatedAt", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @PastOrPresent(message = "Thời gian cập nhật không thể ở tương lai")
+    @Column(name = "UpdatedAt")
+    private LocalDateTime updatedAt;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "Role", nullable = false)
@@ -62,8 +68,9 @@ public class User implements UserDetails {
 
     @Size(max = 255, message = "Đường dẫn ảnh đại diện không được vượt quá 255 ký tự")
     @Pattern(
-            regexp = "^[a-zA-Z0-9_\\-./]+\\.(jpg|jpeg|png|gif|webp|bmp|svg)$",
-            message = "Tên file ảnh không hợp lệ hoặc định dạng không được hỗ trợ"
+            regexp = "^$|.*\\.(jpg|jpeg|png|gif|webp|bmp|svg)$",
+            message = "Tên file ảnh không hợp lệ hoặc định dạng không được hỗ trợ",
+            flags = Pattern.Flag.CASE_INSENSITIVE
     )
     @Column(name = "ProfilePicture", length = 255)
     private String profilePicture;
@@ -75,8 +82,14 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+
+        if (this.role == null) {
+            return List.of(); // Trả về rỗng nếu role null (không mong muốn)
+        }
+        // Ví dụ: Role.SUPER_ADMIN.name() sẽ là "SUPER_ADMIN", sau đó thành "ROLE_SUPER_ADMIN"
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
     }
+
 
     @Override
     public String getPassword() {
@@ -85,6 +98,6 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return this.email;
+        return this.username;
     }
 }
